9e551786bd1a1e242b0469a3126ae13ab5bd619a
JinleeJeong <45163013+JinleeJeong@users.noreply.github.com>
2019-04-15 18:26:00 +0900

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -25,6 +25,7 @@ models.sequelize.sync().then( () => {
   console.log("✗ DB connection error. Please make sure DB is running.")
   console.log(err)
 });
+
 app.use(logger('dev'));
 app.use(express.json({limit: '16mb'}));
 app.use(express.urlencoded({ limit: '16mb', extended: false }));
