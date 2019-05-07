0035431c17ee854a447db8440396cede02144ad7
JinleeJeong <45163013+JinleeJeong@users.noreply.github.com>
2019-05-07 22:06:13 +0900

    Update appverinfo.js

--- backend/routes/appverinfo.js
+++ backend/routes/appverinfo.js
@@ -27,7 +27,7 @@ function encrypt(text){
 
 /* 암호화에서 문자열 16자 이하면, update는 null값을 가진다. 
  항상 update + final 형식으로 암호화를 해야한다.
-*** Key값은 클라이언트에 노출되지 않도록 한다. *** */
+*** Key값은 클라이언트에 노출되지 않도록 한다. ***/
 function decrypt(text){
     if(typeof text == 'string' && text !== null && text !== 'null'){
         var decipher = crypto.createDecipheriv('aes-256-cbc', key.crtSecret, ivBuffer);
